013d6ea23103a80f437b4855c692044e78f77ef1
Juan Augusto Paredes <jparedes@umich.edu>
2025-06-15 11:39:20 -0400

    Updated plot animation file.

--- make_plot_animation_w_video.m
+++ make_plot_animation_w_video.m
@@ -28,28 +28,28 @@ set(gcf,'position',[400,100,600,400])
 
 for ii = 1:length(yy)
 
-set(gcf, 'color', [1 1 1])
-plot(tt(1:ii),yy(1:ii),'color',[0 0.4470 0.7410],'linewidth',2)
-xlim([tt(1) tt(end)])
-ylim([0 1.1])
-
-set(gca,'TickLabelInterpreter','latex')
-ax = gca;
-ax.FontSize = fontAxisLabels; 
-ylabel('$y$' , 'interpreter', 'latex','fontsize', fontLatexLabels)  
-xlabel('$t$ (s)' , 'interpreter', 'latex', 'fontsize', fontLatexLabels)
-grid on
-box on
-
-set(gca,'position',[0.15,0.2,0.82,0.75])
-
-if vidWrite > 0.5
-    currFrame = getframe(gcf);
-    writeVideo(vidObj,currFrame);
-else
-    drawnow
-    pause(0.05)
-end
+    set(gcf, 'color', [1 1 1])
+    plot(tt(1:ii),yy(1:ii),'color',[0 0.4470 0.7410],'linewidth',2)
+    xlim([tt(1) tt(end)])
+    ylim([0 1.1])
+    
+    set(gca,'TickLabelInterpreter','latex')
+    ax = gca;
+    ax.FontSize = fontAxisLabels; 
+    ylabel('$y$' , 'interpreter', 'latex','fontsize', fontLatexLabels)  
+    xlabel('$t$ (s)' , 'interpreter', 'latex', 'fontsize', fontLatexLabels)
+    grid on
+    box on
+    
+    set(gca,'position',[0.15,0.2,0.82,0.75])
+    
+    if vidWrite > 0.5
+        currFrame = getframe(gcf);
+        writeVideo(vidObj,currFrame);
+    else
+        drawnow
+        pause(0.05)
+    end
 
 end
 
